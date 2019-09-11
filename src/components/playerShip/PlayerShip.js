a475f0b1a15a11dabea02bc09b86b39947f3654e
Anthony Moss <asmoss93@gmail.com>
2019-09-10 20:40:03 -0400

    adds broken movement of playership using awsd or arrow keys

--- src/components/playerShip/PlayerShip.js
+++ src/components/playerShip/PlayerShip.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { RegularPolygon, Wedge } from 'react-konva'
+import { Wedge } from 'react-konva'
 
 
 class SpaceShip extends React.Component {
@@ -8,31 +8,55 @@ class SpaceShip extends React.Component {
     
         this.state={
             color: 'purple',
-            startX: 500,
-            startY: 400,
+            x: props.x,
+            y: props.y,
             angle: 50,
-            rotation: 65,
-            currentX: null,
-            currentY: null
+            rotation: props.rotation,
+            speed: 8
+            
         }
     }
 
     static getDerivedStateFromProps(props, state) {
         let keyValue = props.keyPress;
         let rotation = state.rotation;
+        let speed = state.speed
+        console.log(keyValue)
+
+        let x = state.x - Math.sin(-(rotation - 65)*Math.PI/180) * speed;
+        let y = state.y - Math.cos(-(rotation - 65)*Math.PI/180) * speed;
+        const change = {
+            x: state.x,
+            y: state.y,
+            rotation
+        }
+        while (keyValue.left) {
+            change.rotation -= 9
+            return change
+        }
+        while (keyValue.right) {
+            change.rotation += 9
+            return change
+        }
+        while (keyValue.up) {
+            change.x = x
+            change.y = y
+            return change
+        }
+        
     }
 
     render() {
         return (
             <Wedge
-                x={600}
-                y={400}
+                x={this.state.x}
+                y={this.state.y}
                 radius={35}
                 fill={this.state.color}
                 stroke={'grey'}
                 strokeWidth={1}
-                angle={50}
-                rotation={65}
+                angle={this.state.angle}
+                rotation={this.state.rotation}
                 shadowColor= {'yellow'}
                 shadowBlur={7}
                 ref={node => this.wedge = node}
@@ -41,7 +65,44 @@ class SpaceShip extends React.Component {
                     }}
             />
         )
+        
+        
+    }
+    handleRotation = () => {
+        let rotation = this.state.rotation
+
+        this.setState({
+            ...this.state,
+
+        })
     }
 }
 
 export default SpaceShip
+
+
+
+
+
+
+
+        // switch (keyValue) {
+        //     case left: return { rotation: rotation - 9 }
+        //         break;
+        //     case right: return { rotation: rotation + 9 }
+        //         break;
+        //     case forward: return { x, y }
+        //         break;
+        // }
+        // const change = { x, y, rotation }
+        // if (keyValue.left == true) {
+        //     return rotation - 9
+
+        // } else if (keyValue.right == true) {
+        //     change.roation = rotation + 9
+        //     return change
+        // } else if (keyValue.up == true) {
+        //     change.x = x
+        //     change.y = y
+        //     return change
+        // }
