a4b60ba2da1559c81a6c8d644e1130127c6fd825
Anthony Moss <asmoss93@gmail.com>
2019-09-18 17:52:24 -0400

    adds asteroids into own group for hit detection later on

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Stage, Layer } from 'react-konva';
+import { Stage, Layer, Group } from 'react-konva';
 import Gamescreen from './components/gameScreen/GameScreen';
 import SpaceShip from './components/playerShip/PlayerShip';
 import Astroids from './components/Asteroids.js';
@@ -34,30 +34,39 @@ class App extends React.Component {
     }
   }
 
+  componentDidMount() {
+    // console.log()
+    // let layer = this.asteroid.getLayer()
+  }
+
   render() {
     let spaceship = this.state.spaceship.map((ship, i) => {
-      return <SpaceShip key={i} x={600} y={400} width={this.state.screen.width} height={this.state.screen.height} rotation={65} speed={8} keyPress={this.state.keys} ref={node => {
+      return <SpaceShip key={i} x={600} y={400} width={this.state.screen.width} height={this.state.screen.height} rotation={65} speed={8} ref={node => {
         this.spaceShipNode = node;
       }}/>
     })
 
     let asteroids = this.state.asteroids.map((asteroid, i) => {
-      return <Astroids key={i} x={asteroid.x} y={asteroid.y} />
+      return <Astroids key={i} x={asteroid.x} y={asteroid.y} refref={node => {
+        this.asteroid = node;}}/>
     })
+
 
     return (
       <div tabIndex='0' onKeyDown={(event) => {
-        console.log(event.key)
+        // console.log(event.key)
       }}>
       <Stage width={window.innerWidth} height={window.innerHeight}>
         <Layer>
           <Gamescreen />
-          {asteroids}
+          <Group key='asteroids'>
+            {asteroids}
+          </Group>
           {spaceship}
         </Layer>
       </Stage>
       </div>
-    );    
+    );
   }
 }
 
