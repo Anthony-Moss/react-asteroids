db1562dfeaa480ccd7f1261eb634ee2281b00a29
Anthony Moss <asmoss93@gmail.com>
2019-09-19 20:50:56 -0400

    simple component that will be used for games background

---
+++ src/components/Background.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Rect, Wedge } from 'react-konva';
+
+
+
+require('./background.css')
+
+class Background extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            color: 'black',
+        }
+    }
+
+
+    render() {
+        return (
+            <Rect
+                x={20}
+                y={20}
+                width={1200}
+                height={800}
+                fill={this.state.color}
+            />
+        )
+    }
+}
+
+export default Background
