30331d39c9ba33f6ad7eab82146a25b5a9e542a5
Anthony Moss <asmoss93@gmail.com>
2019-09-19 20:46:09 -0400

    turns Asteroids into a simple dumb component to use with redux

--- src/components/Asteroids.js
+++ src/components/Asteroids.js
@@ -1,57 +1,45 @@
 import React from 'react';
-import { RegularPolygon, Line } from 'react-konva';
+import { RegularPolygon, Circle } from 'react-konva';
 import Konva from 'konva';
+import { connect } from 'react-redux';
 
 class Asteroids extends React.Component {
     constructor(props) {
         super(props)
-
-        let getSides = () => {
-            let min = 5;
-            const max = 10;
-            const sides = Math.floor(Math.random() * (max - min + 1) + min)
-            return sides
-        }
-
-        let getRadius = () => {
-            let min = 10;
-            const max = 75;
-            const radius = Math.floor(Math.random() * (max - min + 1) + min)
-            return radius
-        }
-
-        this.state = {
-            numOfSides: getSides(),
-            radius: getRadius(),
-            x: props.x,
-            y: props.y,
-            speed: 1
-        }
     }
 
     
-    componentDidMount() {
-        let angularSpeed = 60;
-        this.anim = new Konva.Animation(frame => {
-        let angleDiff = (frame.timeDiff * angularSpeed) / 1000;
-        this.asteroid.rotate(angleDiff);
-        this.asteroid.cache();
-        }, this.asteroid.getLayer());
-        this.anim.start();
-    }
+    // componentDidMount() {
+    //     let angularSpeed = 60;   
+    //     this.anim = new Konva.Animation(frame => {
+    //         let angleDiff = (frame.timeDiff * angularSpeed) / 1000;
+    //         this.asteroid.rotate(angleDiff);
+    //         this.asteroid.cache();
+    //     }, this.asteroid.getLayer());
+    //     this.anim.start();
+    // }
+
 
     render () {
         return (
             <RegularPolygon
-            x={this.state.x}
-            y={this.state.y}
-            sides={this.state.numOfSides}
-            radius={this.state.radius}
-            stroke="white"
-            ref={node => this.asteroid = node}
-        />
+                x={this.props.x}
+                y={this.props.y}
+                sides={this.props.numOfSides}
+                radius={this.props.radius}
+                fill="green"
+                stroke="white"
+                ref={node => this.asteroid = node}            
+            />
         )
     }
 }
 
-export default Asteroids;
+function mapStateToProps(state) {
+    console.log(state)
+    return {
+        ...state.asteroids
+    }
+}
+
+export default connect(mapStateToProps)(Asteroids);
