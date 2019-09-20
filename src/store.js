dc1329de20fa43ffbfa7903de35c149d87a6ab06
Anthony Moss <asmoss93@gmail.com>
2019-09-19 20:50:01 -0400

    adds playerReducer and asteroidReducer to the redux store

--- src/store.js
+++ src/store.js
@@ -1,15 +1,16 @@
-import React from 'react';
-import { render } from 'react-dom';
 import { createStore, combineReducers } from 'redux';
+import asteroidsReducer from './reducers/asteroidsReducer';
+import playerReducer from './reducers/playerReducer';
 
 
 const rootReducer = combineReducers({
-    
+    player: playerReducer,
+    asteroids: asteroidsReducer
 })
 
 const store = createStore(
     rootReducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-)
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 export default store
