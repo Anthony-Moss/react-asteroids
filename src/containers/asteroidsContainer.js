7f6989e01e98a6449f36769b9dc43c6b91cc246c
Anthony Moss <asmoss93@gmail.com>
2019-09-24 19:23:25 -0400

    comments out code as this is no longer used but keeping incase change mind later

--- src/containers/asteroidsContainer.js
+++ src/containers/asteroidsContainer.js
@@ -1,28 +1,29 @@
-import { connect } from 'react-redux';
-import Asteroids from '../components/Asteroids';
+// import { connect } from 'react-redux';
+// import Asteroids from '../components/Asteroids';
 
-const mapStateToProps = (state) => {
-    return {
-        x: state.asteroids.x,
-        y: state.asteroids.y,
-        numOfSides: state.asteroids.numOfSides,
-        radius: state.asteroids.radius
-    }
-}
+// const mapStateToProps = (state) => {
+//     return {
+//             x: state.asteroids[0].x,
+//             y: state.asteroids[0].y,
+//             numOfSides: state.asteroids[0].numOfSides,
+//             radius: state.asteroids[0].radius
+//         }
+    
+// }
 
-const mapDispatchToProps = (dispatch) => {
-    return {
-        asteroidCoords: (asteroidCoords) => {
-            dispatch({
-                type: 'ASTEROIDS_MOVE',
-                payload: {
-                    // asteroids new coords should go here
-                }
-            })
-        }
-    }
-}
+// const mapDispatchToProps = (dispatch) => {
+//     return {
+//         asteroidCoords: (asteroidCoords) => {
+//             dispatch({
+//                 type: 'ASTEROIDS_MOVE',
+//                 payload: {
+//                     // asteroids spinning will go here
+//                 }
+//             })
+//         }
+//     }
+// }
 
-const makeAsteroidsSmart = connect(mapStateToProps, mapDispatchToProps);
-const smartAsteroids = makeAsteroidsSmart(Asteroids);
-export default smartAsteroids
+// const makeAsteroidsSmart = connect(mapStateToProps, mapDispatchToProps);
+// const smartAsteroids = makeAsteroidsSmart(Asteroids);
+// export default smartAsteroids
