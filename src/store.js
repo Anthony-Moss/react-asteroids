3caae6e59b4359b908c1cbed8dd99d9c308985a7
Anthony Moss <asmoss93@gmail.com>
2019-09-24 19:21:52 -0400

    removes asteroid reducer, as its not needed, the state is tracked in game

--- src/store.js
+++ src/store.js
@@ -1,12 +1,12 @@
 import { createStore, combineReducers } from 'redux';
-import asteroidsReducer from './reducers/asteroidsReducer';
+// import asteroidsReducer from './reducers/asteroidsReducer';
 import playerReducer from './reducers/playerReducer';
 import gameReducer from './reducers/gameReducer'
 
 
 const rootReducer = combineReducers({
     player: playerReducer,
-    asteroids: asteroidsReducer,
+    // asteroids: asteroidsReducer,
     game: gameReducer
 })
 
