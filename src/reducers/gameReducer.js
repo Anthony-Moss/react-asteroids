3630859050fba112c56f405a7c5756725a1fc035
Anthony Moss <asmoss93@gmail.com>
2019-09-24 18:27:44 -0400

    adds reducer for game, has one case for now, it adds an asteroid to the game on press of a button and makes asteroids state empty as initial bc no asteroids to start

--- src/reducers/gameReducer.js
+++ src/reducers/gameReducer.js
@@ -1,17 +1,56 @@
+import { getRadius, getSides, getX, getY } from './asteroidsReducer'
+
 const initialState = {
     screen: {
         width: window.innerWidth,
         height: window.innerHeight,
         ratio: window.devicePixelRatio || 1,
     },
-    asteroids: [{}, {}, {}]
+    asteroids: []
 }
 
 const gameReducer = (state=initialState, action) => {
     switch(action.type) {
+        case "START_GAME":
+
+            return {
+                ...state,
+                asteroids: [{
+                    numOfSides: getSides(),
+                    radius: getRadius(),
+                    x: getX(),
+                    y: getY(),
+                    speed: 1
+                }]
+            }
         default:
             return state
     }
 }
 
 export default gameReducer
+
+
+
+
+
+
+// screen: {
+//     width: window.innerWidth,
+//     height: window.innerHeight,
+//     ratio: window.devicePixelRatio || 1,
+// },
+// asteroids: [{
+//     numOfSides: getSides(),
+//     radius: getRadius(),
+//     x: getX(),
+//     y: getY(),
+//     speed: 1
+// },
+// {
+//     numOfSides: getSides(),
+//     radius: getRadius(),
+//     x: getX(),
+//     y: getY(),
+//     speed: 1
+// }]
