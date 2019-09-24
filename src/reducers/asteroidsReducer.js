05bfd2c284d05c4540c4574925f1a123a73cb46e
Anthony Moss <asmoss93@gmail.com>
2019-09-24 19:26:45 -0400

    comments out the code for the reducer as there is not a state for them, they are controlled by game component

--- src/reducers/asteroidsReducer.js
+++ src/reducers/asteroidsReducer.js
@@ -1,48 +1,57 @@
-let getSides = () => {
+export let getSides = () => {
     let min = 5;
     const max = 10;
     const sides = Math.floor(Math.random() * (max - min + 1) + min)
     return sides
 }
 
-let getRadius = () => {
+export let getRadius = () => {
     let min = 10;
     const max = 75;
     const radius = Math.floor(Math.random() * (max - min + 1) + min)
     return radius
 }
 
-let getX = () => {
+export let getX = () => {
     let min = 50;
     const max = 500;
     const x = Math.floor(Math.random() * (max - min + 1) + min)
     return x
 }
 
-let getY = () => {
+export let getY = () => {
     let min = 50;
     const max = 500;
     const y = Math.floor(Math.random() * (max - min + 1) + min)
     return y
 }
 
-const initialState = {
-    numOfSides: getSides(),
-    radius: getRadius(),
-    x: getX(),
-    y: getY(),
-    speed: 1
-}
+// const initialState = []
+//     // [{
+//     //     numOfSides: getSides(),
+//     //     radius: getRadius(),
+//     //     x: getX(),
+//     //     y: getY(),
+//     //     speed: 1
+//     // },
+//     // {
+//     //     numOfSides: getSides(),
+//     //     radius: getRadius(),
+//     //     x: getX(),
+//     //     y: getY(),
+//     //     speed: 1
+//     // }]
 
-const asteroidsReducer = (state=initialState, action) => {
-    switch(action.type) {
-        case '':
-            return {
-                ...state
-            }
-        default:
-            return state 
-    }
-}
 
-export default asteroidsReducer
+// const asteroidsReducer = (state=initialState, action) => {
+//     switch(action.type) {
+//         case '':
+//             return {
+//                 ...state
+//             }
+//         default:
+//             return state 
+//     }
+// }
+
+// export default asteroidsReducer
